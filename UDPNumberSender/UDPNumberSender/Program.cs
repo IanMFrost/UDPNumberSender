e16520f1b64c4c71a8504135e1e8fa7f052b3026
IanMFrost <ianmfrost0@gmail.com>
2018-03-14 10:54:22 +0100

    UDPNumberSender projekt oprettet

---
+++ UDPNumberSender/UDPNumberSender/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPNumberSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+
+            IPAddress ip = IPAddress.Parse("127.0.0.1"); //
+            UdpClient udpClient = new UdpClient("127.0.0.1", 9999);
+
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 9999); 
+
+            while(true)
+            {
+                Console.WriteLine("The number is:" + number);
+                number++;
+            }
+            //udpClient.Connect(RemoteIpEndPoint); //
+        }
+    }
+}
